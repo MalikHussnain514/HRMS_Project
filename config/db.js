27d69c381bc0ae0d716bbaf14992703ed306959a
Ali Hussnain <ali.hussnain@appsgenii.eu>
2023-05-26 15:41:35 +0500

    changes in start script and database connect file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.set("strictQuery", false);
-
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGOURI, {
