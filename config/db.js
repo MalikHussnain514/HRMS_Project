06faad36ae45fa0e280d2aaf2e5ce9d7c997b70a
Ali Hussnain <ali.hussnain@appsgenii.eu>
2023-05-26 13:39:37 +0500

    resole the database connection error

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGOURI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
+      // useCreateIndex: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
+      // useFindAndModify: false,
     });
 
     console.log(`DataBase Connected at ${connect.connection.host}`);
   } catch (error) {
-    console.log("DataBase connection error");
+    console.log("DataBase connection error", error.message);
     process.exit(1);
   }
 };
